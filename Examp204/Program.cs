4d8bba891375150d22375ddd7373ee9c1255d32c
TimofeyKornilin <you@example.com>
2023-05-30 13:34:06 +0700

    Array and find element and index

---
+++ Examp204/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 14, 2, 7, 35, 43, 45, 36, 7, 18, 9 };
+
+int n = array.Length;
+int find = 7;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
